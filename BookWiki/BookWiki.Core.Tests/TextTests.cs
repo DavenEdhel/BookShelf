3f889d543033150b62e6ac958f481d5e2d7ab4cd
Stanislau_Kanaplianik <Stanislau.Kanaplianik.contractor@keurig.com>
2019-05-30 09:20:42 +0300

    Test

--- BookWiki/BookWiki.Core.Tests/TextTests.cs
+++ BookWiki/BookWiki.Core.Tests/TextTests.cs
@@ -10,11 +10,9 @@ namespace BookWiki.Core.Tests
             var text = new StringText("0123456789");
 
             var s1 = text.Substring(3, 5);
-
             Assert.AreEqual("34567", s1.PlainText);
 
             var s2 = s1.Substring(2, 3);
-
             Assert.AreEqual("567", s2.PlainText);
         }
 
